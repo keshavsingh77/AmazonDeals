f83e36bd99a2d765742dd1972ae43a5e6fb3bd90
keshavsingh77 <keshavrajlkr2@gmail.com>
2025-12-27 10:22:46 +0530

    Links updated via Web Admin

--- links.js
+++ links.js
@@ -1,9 +1,7 @@
-const amazonData = {
-  "affiliate_tag": "dukankasaman-21",
-  "total": 2,
-  "updated": "27/12/2025, 10:17:21 am",
-  "products": [
+const AFFILIATE_TAG = "dukankasaman-21"; // Apna Tag Yahan Daalo
+
+const products = [
     "https://www.amazon.in/dp/B08BYBP536?th=1&tag=dukankasaman-21",
-    "https://www.amazon.in/dp/B0FKTGWWSH?psc=1&th=1&tag=dukankasaman-21"
-  ]
-};
+    "https://www.amazon.in/dp/B0FKTGWWSH?psc=1&th=1&tag=dukankasaman-21",
+    // Aur links yahan comma (,) laga ke add karte jao
+];
